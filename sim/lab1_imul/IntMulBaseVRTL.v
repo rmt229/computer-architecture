5712953eeb06426782c1dddf02d59b40e7c41b6d
hq38 <hq38@cornell.edu>
2020-09-13 09:44:11 -0400

    add datapath in base model and add some random tests

--- sim/lab1_imul/IntMulBaseVRTL.v
+++ sim/lab1_imul/IntMulBaseVRTL.v
@@ -34,6 +34,130 @@ module lab1_imul_IntMulBaseVRTL
   // together.
   // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+// Datapath 
+  // Split out the a and b operands 
+
+  logic [63:0] req_msg_a = req_msg[63:12];
+  logic [63:0] req_msg_b = req_msg[31:0 ];
+
+  // A Mux 
+  logic [31:0] a_shift_out;
+  logic [31:0] a_mux_out;
+
+  vc_Mux3#(32) a_mux
+  (
+    .sel   (a_mux_sel),
+    .in0   (a_shift_out),
+    .in1   (req_msg_a),
+    .out   (a_mux_out)
+  );
+
+  // A register
+
+  logic [31:0] a_reg_out;
+
+  vc_EnReg#(32) a_reg
+  (
+    .clk   (clk),
+    .reset (reset),
+    .d     (a_mux_out),
+    .q     (a_reg_out)
+  );
+
+  // B Mux
+  logic [31:0] b_shift_out;
+  logic [31:0] b_mux_out;
+
+  vc_Mux3#(32) b_mux
+  (
+    .sel   (b_mux_sel),
+    .in0   (b_shift_out),
+    .in1   (req_msg_b),
+    .out   (b_mux_out)
+  );
+
+  // B register
+
+  logic [31:0] b_reg_out;
+
+  vc_EnReg#(32) b_reg
+  (
+    .clk   (clk),
+    .reset (reset),
+    .d     (b_mux_out),
+    .q     (b_reg_out)
+  );
+
+  // B shift right logical
+
+  vc_RightLogicalShifter#(32) b_right_shifter
+  (
+    .in    (b_reg_out),
+    .out   (b_shift_out),
+    .shamt (1) // TODO: check bit
+  );
+
+  // A shift left logical
+
+  vc_LeftLogicalShifter#(32) a_left_shifter
+  (
+    .in    (a_reg_out),
+    .out   (a_shift_out),
+    .shamt (1) // TODO: check bit
+  );
+
+  // Adder 
+
+  vc_Adder#(32) add
+  (
+    .in0   (a_reg_out),
+    .in1   (result_reg_out),
+    .out   (add_out)
+  );
+
+  // Add mux
+
+  logic [31:0] add_out;
+  logic [31:0] result_reg_out;
+
+  vc_Mux3#(32) add_mux
+  (
+    .sel   (add_mux_sel),
+    .in0   (add_out),
+    .in1   (result_reg_out),
+    .out   (add_mux_out)
+  );
+
+  // Result mux
+
+  logic [31:0] add_out;
+  logic [31:0] result_mux_out;
+
+  vc_Mux3#(32) result_mux
+  (
+    .sel   (result_mux_sel),
+    .in0   (add_mux_out),
+    .in1   (0), // TODO: check if should be r0
+    .out   (result_mux_out)
+  );
+
+  // Result register
+
+  logic [31:0] result_reg_out;
+
+  vc_EnReg#(32) result_reg
+  (
+    .clk   (clk),
+    .reset (reset),
+    .en    (result_en),
+    .d     (result_mux_out),
+    .q     (result_reg_out)
+  );
+
+  // Connect to output port
+  
+  assign resp_msg = result_reg_out;
+  
   //----------------------------------------------------------------------
   // Line Tracing
   //----------------------------------------------------------------------
