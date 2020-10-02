ffb3bcc199ed14db2066e829d9b455994585dab7
joyhuan <joy_huanqi@outlook.com>
2020-10-01 23:16:40 -0500

    add rr operations to pass added sub tests

--- sim/lab2_proc/ProcDpathComponentsVRTL.v
+++ sim/lab2_proc/ProcDpathComponentsVRTL.v
@@ -59,6 +59,16 @@ module lab2_proc_AluVRTL
 
     case ( fn )
       4'd0    : out = in0 + in1;                                // ADD
+      4'd1    : out = in0 - in1;                                // SUB
+      4'd2    : out = in0 * in1;                                // MUL
+      4'd3    : out = in0 & in1;                                // AND
+      4'd4    : out = in0 | in1;                                // OR
+      4'd5    : out = in0 ^ in1;                                // XOR
+      4'd6    : out = in0 < in1;                                // SLT
+      // 4'd7    : out = in0 + in1;                                // SLTU
+      4'd8    : out = in0 >>> in1;                              // SRA
+      4'd9    : out = in0 >> in1;                               // SRL
+      4'd10   : out = in0 << in1;
       4'd11   : out = in0;                                      // CP OP0
       4'd12   : out = in1;                                      // CP OP1
 
--- sim/lab2_proc/test/inst_sub.py
+++ sim/lab2_proc/test/inst_sub.py
@@ -46,3 +46,113 @@ def gen_basic_test():
 # ''' LAB TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # Define additional directed and random test cases.
 # '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+#-------------------------------------------------------------------------
+# gen_dest_dep_test
+#-------------------------------------------------------------------------
+
+def gen_dest_dep_test():
+  return [
+    gen_rr_dest_dep_test( 5, "sub", 1, 1, 0 ),
+    gen_rr_dest_dep_test( 4, "sub", 2, 1, 1 ),
+    gen_rr_dest_dep_test( 3, "sub", 3, 1, 2 ),
+    gen_rr_dest_dep_test( 2, "sub", 4, 1, 3 ),
+    gen_rr_dest_dep_test( 1, "sub", 5, 1, 4 ),
+    gen_rr_dest_dep_test( 0, "sub", 6, 1, 5 ),
+  ]
+
+#-------------------------------------------------------------------------
+# gen_src0_dep_test
+#-------------------------------------------------------------------------
+
+def gen_src0_dep_test():
+  return [
+    gen_rr_src0_dep_test( 5, "sub",  7, 1, 6 ),
+    gen_rr_src0_dep_test( 4, "sub",  8, 1, 7 ),
+    gen_rr_src0_dep_test( 3, "sub",  9, 1, 8 ),
+    gen_rr_src0_dep_test( 2, "sub", 10, 1, 9 ),
+    gen_rr_src0_dep_test( 1, "sub", 11, 1, 10 ),
+    gen_rr_src0_dep_test( 0, "sub", 12, 1, 11 ),
+  ]
+
+#-------------------------------------------------------------------------
+# gen_src1_dep_test
+#-------------------------------------------------------------------------
+
+def gen_src1_dep_test():
+  return [
+    gen_rr_src1_dep_test( 5, "sub", 1, 13, 14 ),
+    gen_rr_src1_dep_test( 4, "sub", 1, 14, 15 ),
+    gen_rr_src1_dep_test( 3, "sub", 1, 15, 16 ),
+    gen_rr_src1_dep_test( 2, "sub", 1, 16, 17 ),
+    gen_rr_src1_dep_test( 1, "sub", 1, 17, 18 ),
+    gen_rr_src1_dep_test( 0, "sub", 1, 18, 19 ),
+  ]
+
+#-------------------------------------------------------------------------
+# gen_srcs_dep_test
+#-------------------------------------------------------------------------
+
+def gen_srcs_dep_test():
+  return [
+    gen_rr_srcs_dep_test( 5, "sub", 12, 2, 14 ),
+    gen_rr_srcs_dep_test( 4, "sub", 13, 3, 16 ),
+    gen_rr_srcs_dep_test( 3, "sub", 14, 4, 18 ),
+    gen_rr_srcs_dep_test( 2, "sub", 15, 5, 20 ),
+    gen_rr_srcs_dep_test( 1, "sub", 16, 6, 22 ),
+    gen_rr_srcs_dep_test( 0, "sub", 17, 7, 24 ),
+  ]
+
+#-------------------------------------------------------------------------
+# gen_srcs_dest_test
+#-------------------------------------------------------------------------
+
+def gen_srcs_dest_test():
+  return [
+    gen_rr_src0_eq_dest_test( "sub", 25, 1, 26 ),
+    gen_rr_src1_eq_dest_test( "sub", 26, 1, 27 ),
+    gen_rr_src0_eq_src1_test( "sub", 27, 54 ),
+    gen_rr_srcs_eq_dest_test( "sub", 28, 56 ),
+  ]
+
+#-------------------------------------------------------------------------
+# gen_value_test
+#-------------------------------------------------------------------------
+
+def gen_value_test():
+  return [
+
+    gen_rr_value_test( "sub", 0x00000000, 0x00000000, 0x00000000 ),
+    gen_rr_value_test( "sub", 0x00000001, 0x00000001, 0x00000000 ),
+    gen_rr_value_test( "sub", 0x00000007, 0x00000003, 0x00000004 ),
+
+    gen_rr_value_test( "sub", 0xffff8000, 0x00000000, 0xffff8000 ),
+    gen_rr_value_test( "sub", 0x80000000, 0x00000000, 0x80000000 ),
+    # gen_rr_value_test( "sub", 0x80000000, 0xffff8000, 0x7fff8000 ),
+
+    gen_rr_value_test( "sub", 0x7fffffff, 0x00000000, 0x7fffffff ),
+    # gen_rr_value_test( "sub", 0x7fffffff, 0x00007fff, 0x80007ffe ),
+
+    # gen_rr_value_test( "sub", 0x80000000, 0x00007fff, 0x80007fff ),
+    # gen_rr_value_test( "sub", 0x7fffffff, 0xffff8000, 0x7fff7fff ),
+
+    gen_rr_value_test( "sub", 0xffffffff, 0x00000000, 0xffffffff ),
+    # gen_rr_value_test( "sub", 0xffffffff, 0x00000001, 0x00000000 ),
+    gen_rr_value_test( "sub", 0xffffffff, 0xffffffff, 0x00000000 ),
+
+  ]
+
+#-------------------------------------------------------------------------
+# gen_random_test
+#-------------------------------------------------------------------------
+
+def gen_random_test():
+  asm_code = []
+  for i in xrange(100):
+    src0 = Bits( 32, random.randint(0,0xffffffff) )
+    src1 = Bits( 32, random.randint(0,0xffffffff) )
+    dest = src0 - src1
+    asm_code.append( gen_rr_value_test( "sub", src0.uint(), src1.uint(), dest.uint() ) )
+  return asm_code
+
