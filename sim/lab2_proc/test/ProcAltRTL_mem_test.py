82606557baef79bedcc8cb26d3b58de2383ba852
zhangzi1 <terencezhangziyi@gmail.com>
2020-10-09 20:55:42 -0400

    add tests to alt as well

--- sim/lab2_proc/test/ProcAltRTL_mem_test.py
+++ sim/lab2_proc/test/ProcAltRTL_mem_test.py
@@ -37,7 +37,13 @@ def test_lw_rand_delays( dump_vcd ):
 import inst_sw
 
 @pytest.mark.parametrize( "name,test", [
-  asm_test( inst_sw.gen_basic_test     ),
+  asm_test( inst_sw.gen_basic_test        ),
+  asm_test( inst_sw.gen_dest_dep_test     ),
+  asm_test( inst_sw.gen_base_dep_test     ),
+  asm_test( inst_sw.gen_base_eq_dep_test  ),
+  asm_test( inst_sw.gen_value_test        ),
+  asm_test( inst_sw.gen_random_test       ),
+  asm_test( inst_sw.gen_back_to_back_test ),
 
   # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   # Add more rows to the test case table to test more complicated
